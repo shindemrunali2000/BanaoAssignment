6c0f40e2aa9b4fbf18ec4a61cbed498d4fbad34a
shindemrunali2000 <shindemrunali2000@gmail.com>
2024-07-15 21:19:29 +0530

    Adding blog code

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BlogPostViewSet, blog_detail
+from .views import BlogPostViewSet
 from .views import patient_dashboard
 
 router = DefaultRouter()
@@ -13,8 +13,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('create/', views.create_blog_post, name='create_blog_post'),
     path('doctor_posts/', views.doctor_blog_posts, name='doctor_blog_posts'),
-    path('category/<str:category_name>/', views.blog_posts_by_category, name='blog_posts_by_category'),
-    # path('category/', views.blog_posts_by_category, name='blog_posts_by_category'),
-    path('dashboard/patient/', patient_dashboard, name='patient_dashboard'),
-    path('post/<int:id>/', blog_detail, name='blog_detail'),
+    # path('category/<str:category_name>/', views.blog_posts_by_category, name='blog_posts_by_category'),
+    # path('dashboard/patient/', patient_dashboard, name='patient_dashboard'),
+    path('post/<int:pk>/', views.blog_detail, name='blog_detail'),
+    path('dashboard/', views.patient_dashboard, name='patient_dashboard'),
 ]
--- newapp/urls.py
+++ newapp/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     path('register/', views.register, name='register.html'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    # path('patient_dashboard/', views.patient_dashboard, name='patient_dashboard'),
+    
 
 ]
 
-#  if settings.DEBUG:
-    #  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
