e572a6c54f8c34179bb9a6fb3c4888a869a96785
shindemrunali2000 <shindemrunali2000@gmail.com>
2024-07-23 17:21:52 +0530

    Appointment Code2 Commit

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.home_page, name='home_page.html'),
@@ -8,6 +11,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
    
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
